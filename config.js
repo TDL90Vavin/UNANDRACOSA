52747e063961d556f5b8d8e308591b4ed691c98f
TDL90Vavin <kettiserve@gmail.com>
2025-10-21 16:07:49 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,13 @@
-window.APP_CONFIG={SUPABASE_URL:'INSERISCI_SUPABASE_URL',SUPABASE_ANON_KEY:'INSERISCI_SUPABASE_ANON_KEY'};
-window._supaClient=()=>{if(!window.__s)window.__s=supabase.createClient(window.APP_CONFIG.SUPABASE_URL,window.APP_CONFIG.SUPABASE_ANON_KEY);return window.__s};
+window.APP_CONFIG = {
+  SUPABASE_URL: 'https://gjmanyvncnezurkximxt.supabase.co',
+  SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdqbWFueXZuY25lenVya3hpbXh0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA2NjE3MzYsImV4cCI6MjA3NjIzNzczNn0.n1KMgjZCUe2OcGiWl5W6DZEzY_bGh31AMAtS1pHmH6s'
+};
+
+window._supaClient = () => {
+  if (!window.__s)
+    window.__s = supabase.createClient(
+      window.APP_CONFIG.SUPABASE_URL,
+      window.APP_CONFIG.SUPABASE_ANON_KEY
+    );
+  return window.__s;
+};
